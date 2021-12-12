bd9119d5dba5740c31cc46787d31fe54f1f71b6d
noam amron <noam0574@gmail.com>
2021-12-12 19:11:51 +0200

    project initialization, finished logic of exercises 1,2,3 and start exercise 4

---
+++ HomeWork 19/scripts/exercise_three.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  const getRandomNumber = (min, max) => {
+    let number = Math.random() * (max - min) + min;
+    if (String(number).substr(0, 1) === "0") {
+      return min;
+    }
+    if (number % 1 < 0.5) {
+      number = Math.floor(number);
+    } else {
+      number = Math.ceil(number);
+    }
+
+    return number;
+  };
+
+  const getRandomColor = () => {
+    let redColor = getRandomNumber(0, 255);
+    let greenColor = getRandomNumber(0, 255);
+    let blueColor = getRandomNumber(0, 255);
+    return `rgb(${redColor}, ${greenColor}, ${blueColor})`;
+  };
+
+  const displayNumbersAndColorOnBox = () => {
+    let randomColor, randomNumber;
+
+    for (let index = 1; index <= 15; index++) {
+      randomNumber = getRandomNumber(0, 100);
+      randomColor = getRandomColor();
+      $(`.box-number-${index}`).css("background-color", randomColor);
+      $(`.box-number-${index}`).text(randomNumber);
+    }
+  };
+  $(".random-number-color-button").click(() => {
+    displayNumbersAndColorOnBox();
+  });
+});
---
+++ HomeWork 19/scripts/exercises_one_and_two_script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  // #1: copy from text input
+  let firstInputElement = $("#first-input");
+  let secondInputElement = $("#second-input");
+
+  $(".first-copy-button").click(() => {
+    // console.log();
+    $(".first-result").text(firstInputElement.val());
+    firstInputElement.remove();
+  });
+  $(".second-input-button").click(() => {
+    // console.log();
+    $(".second-result").text(secondInputElement.val());
+    secondInputElement.remove();
+  });
+
+  //   #2: List of numbers
+  let firstNumberElement = $("#first-number-list-input");
+  let secondNumberElement = $("#second-number-list-input");
+
+  $(".number-list-container").hide();
+  $(".number-list-button").click(() => {
+    $("#number-list").empty();
+    let firstNumber = firstNumberElement.val();
+    let secondNumber = secondNumberElement.val();
+    $("#number-list").append(setOptionsToSelect(firstNumber, secondNumber));
+    $(".number-list-container").show();
+  });
+
+  const setOptionsToSelect = (firstNum, secondNum) => {
+    let optionsStr = "";
+    for (let i = firstNum; i <= secondNum; i++) {
+      optionsStr += `<option value="${i}">${i}</option>`;
+    }
+    return optionsStr;
+  };
+});
